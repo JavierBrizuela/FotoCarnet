d79be41c05dd1c82e3e6a3c7c9c6e4415f3ce686
JavierBrizuela <jbrizuela1976@gmail.com>
2025-04-23 23:12:50 -0300

    fix remove bg with selfie_segmentation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, render_template
-from processImage import image_crop, image_resizer, image_code
+from processImage import image_crop, image_resizer, image_code, remove_background
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,11 +17,11 @@ def process_image():
     height = float(request.form.get('alto'))
     dpi = int(request.form.get('dpi'))
     percent = int(request.form.get('porcentaje'))
-    
-    image_croped = image_crop(file, width, height, dpi, percent)
-    #image_resized = image_resizer(image_croped, width, height, dpi)
-    image_process = image_code(image_croped)
-    
+    bg_color = [255, 128, 64]
+    selfie_segmentation = remove_background(file, bg_color)
+    image_croped = image_crop(selfie_segmentation, width, height, percent)
+    image_resized = image_resizer(image_croped, width, height, dpi)
+    image_process = image_code(image_resized)
     return jsonify({
         'processed_image': image_process,
         'message': 'Procesamiento exitoso'
--- processImage.py
+++ processImage.py
@@ -1,15 +1,17 @@
 import cv2 as cv
 import numpy as np
 import base64
+import mediapipe as mp
 
-def image_crop(file, width, height, dpi, percent):
-    # Leer imagen
-    img = cv.imdecode(np.frombuffer(file.read(), np.uint8), cv.IMREAD_COLOR)
+def image_crop(img , width, height, percent):
+    # Convertir imagen rgb a gris
     img_grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     # Cargar clasificador de rostros
     face_cascade = cv.CascadeClassifier('Data/haarcascade_frontalface_default.xml')
     face = face_cascade.detectMultiScale(img_grey, 1.1, 4)
+    
     if len(face) != 1:
+        
         return None
     x, y, w, h = face[0]
     # Correjir la región de la cara
@@ -26,8 +28,15 @@ def image_crop(file, width, height, dpi, percent):
     y = y - int(height_diff * 0.3)
     w =  (side_margin * 2) + w 
     h =  height_diff + h
-    # Recortar imagen    
-    processed_img = img[y:y+h, x:x+w]
+    # Si la región a recortar está fuera de los límites de la imagen, agregar padding
+    if x < 0 or y < 0 or w > img.shape[1] or h > img.shape[0]:
+        img_croped = image_padding(img, x, y, w, h)
+    else:
+        img_croped = img[y:y + h, x:x + w]
+    return img_croped
+
+def image_padding(img, x, y, w, h):
+    # Asegurarse de que no se salga de los límites de la imagen
     x1 = max(0, x)
     y1 = max(0, y)
     x2 = min(img.shape[1], x + w)
@@ -39,13 +48,67 @@ def image_crop(file, width, height, dpi, percent):
     pad_right = max(0, (x + w) - x2)
     pad_top = max(0, y1 - y)
     pad_bottom = max(0, (h + y) - y2)
-    
     img_padded = cv.copyMakeBorder(img_crop, 
                                    pad_top, pad_bottom, pad_left, pad_right, 
                                    cv.BORDER_CONSTANT, 
                                    value=[0, 0, 0])
     
-    return img_padded
+    # Crear máscara (áreas negras = originales, áreas blancas = a rellenar)
+    mask = np.zeros(img_padded.shape[:2], dtype=np.uint8)
+
+    # Marcar las áreas de padding como blancas (255)
+    if pad_top > 0:
+        mask[:pad_top, :] = 255
+    if pad_bottom > 0:
+        mask[-pad_bottom:, :] = 255
+    if pad_left > 0:
+        mask[:, :pad_left] = 255
+    if pad_right > 0:
+        mask[:, -pad_right:] = 255
+    
+    # Aplicar inpainting (método rápido: cv2.INPAINT_TELEA)
+    inpainted_face = cv.inpaint(
+        img_padded, 
+        mask, 
+        inpaintRadius=3,  # Radio de influencia para el relleno
+        flags=cv.INPAINT_TELEA
+    )
+    return inpainted_face
+
+def remove_background(file , bg_color=[255, 255, 255], blur_amount=15):
+    """
+    Remueve el fondo de una imagen con una persona y lo reemplaza con el color especificado.
+    
+    Args:
+        img: Imagen en formato OpenCV (BGR)
+        bg_color: Color de fondo deseado en formato BGR [B, G, R]
+        blur_amount: Cantidad de desenfoque aplicado a la máscara (debe ser impar)
+    Returns:
+        Imagen con el fondo reemplazado
+    """
+     # Leer imagen
+    img = cv.imdecode(np.frombuffer(file.read(), np.uint8), cv.IMREAD_COLOR)
+    
+    # Inicializar MediaPipe Selfie Segmentation
+    selfie_segmentation = mp.solutions.selfie_segmentation
+    selfie_segmentation = selfie_segmentation.SelfieSegmentation(model_selection=0)
+    result = selfie_segmentation.process(img)
+    # Obtener mascara de segmentación
+    mask = result.segmentation_mask
+    # Aplicar suavizado adaptativo a la máscara para mejorar los bordes
+    blurred = cv.GaussianBlur(mask, (blur_amount, blur_amount), 0)
+    blurred = blurred[:, :, np.newaxis]  # Convertir a 3 canales
+    # Crear fondo con el color de fondo
+    bg_image = np.ones(img.shape, dtype=np.uint8)
+    bg_image[:] = bg_color  # Color en BGR
+    # crear fusion entre la imagen original y el fondo
+    output_image = img * blurred + bg_image * (1 - blurred)
+    output_image = output_image.astype(np.uint8)
+    
+    # Liberar recursos
+    selfie_segmentation.close()
+    
+    return output_image
 
 def image_resizer(file, width, height, dpi):
     
